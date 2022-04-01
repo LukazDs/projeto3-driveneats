8837ebe089505145c986bad3a056dc55fc536c64
Lucas Duarte da Silva <lukazds2016@gmail.com>
2022-04-01 14:21:33 -0300

    Correção da Sexta Versão

--- scripts.js
+++ scripts.js
@@ -1,172 +1,30 @@
-//primeira escolha
-function chooseChicken() {
-    document.getElementById("Frango").style.borderColor = "green";
-    document.getElementById("Carne").style.borderColor = "white";
-    document.getElementById("Lanche").style.borderColor = "white";
-    document.getElementById("Pernil").style.borderColor = "white";
-    document.getElementById("Pao").style.borderColor = "white";
-    document.getElementById("Lagosta").style.borderColor = "white";
-    plate = "Frango"
+let firstfood = ''
+let secondfood = ''
+let thirdfood = ''
+
+function chooseFood(elemento) {
+    const food = document.querySelector(".middle-items-border-one");
+    if(food !== null) {
+        food.classList.remove("middle-items-border-one")
+    }
+    elemento.classList.toggle("middle-items-border-one")
+    firstfood = elemento.querySelector(".middle-first-text").innerHTML
 }
 
-function chooseSteak() {
-    document.getElementById("Frango").style.borderColor = "white";
-    document.getElementById("Carne").style.borderColor = "green";
-    document.getElementById("Lanche").style.borderColor = "white";
-    document.getElementById("Pernil").style.borderColor = "white";
-    document.getElementById("Pao").style.borderColor = "white";
-    document.getElementById("Lagosta").style.borderColor = "white";
-    plate = "Bife"
+function chooseDrink(elemento) {
+    drink = document.querySelector(".middle-items-border-two")
+    if(drink !== null) {
+        drink.classList.remove("middle-items-border-two")
+    }
+    elemento.classList.toggle("middle-items-border-two")
+    secondfood = elemento.querySelector(".middle-first-text").innerHTML
 }
 
-function chooseSnack() {
-    document.getElementById("Frango").style.borderColor = "white";
-    document.getElementById("Carne").style.borderColor = "white";
-    document.getElementById("Lanche").style.borderColor = "green";
-    document.getElementById("Pernil").style.borderColor = "white";
-    document.getElementById("Pao").style.borderColor = "white";
-    document.getElementById("Lagosta").style.borderColor = "white";
-    plate = "Lanche"
-}
-
-function chooseHam() {
-    document.getElementById("Frango").style.borderColor = "white";
-    document.getElementById("Carne").style.borderColor = "white";
-    document.getElementById("Lanche").style.borderColor = "white";
-    document.getElementById("Pernil").style.borderColor = "green";
-    document.getElementById("Pao").style.borderColor = "white";
-    document.getElementById("Lagosta").style.borderColor = "white";
-    plate = "Pernil Assado"
-}
-
-function chooseBread() {
-    document.getElementById("Frango").style.borderColor = "white";
-    document.getElementById("Carne").style.borderColor = "white";
-    document.getElementById("Lanche").style.borderColor = "white";
-    document.getElementById("Pernil").style.borderColor = "white";
-    document.getElementById("Pao").style.borderColor = "green";
-    document.getElementById("Lagosta").style.borderColor = "white";
-    plate = "Pão de Batata"
-}
-
-function chooseLobster() {
-    document.getElementById("Frango").style.borderColor = "white";
-    document.getElementById("Carne").style.borderColor = "white";
-    document.getElementById("Lanche").style.borderColor = "white";
-    document.getElementById("Pernil").style.borderColor = "white";
-    document.getElementById("Pao").style.borderColor = "white";
-    document.getElementById("Lagosta").style.borderColor = "green";
-    plate = "Lagosta"
-}
-
-//segunda escolha
-function chooseCoke() {
-    document.getElementById("coca").style.borderColor = "green";
-    document.getElementById("suco-natural").style.borderColor = "white";
-    document.getElementById("whisky").style.borderColor = "white";
-    document.getElementById("coco").style.borderColor = "white";
-    document.getElementById("vinho").style.borderColor = "white";
-    document.getElementById("agua").style.borderColor = "white";
-    plate = "Coca cola"
-}
-function chooseJuice() {
-    document.getElementById("coca").style.borderColor = "white";
-    document.getElementById("suco-natural").style.borderColor = "green";
-    document.getElementById("whisky").style.borderColor = "white";
-    document.getElementById("coco").style.borderColor = "white";
-    document.getElementById("vinho").style.borderColor = "white";
-    document.getElementById("agua").style.borderColor = "white";
-    plate = "Suco natural"
-}
-function chooseWhisky() {
-    document.getElementById("coca").style.borderColor = "white";
-    document.getElementById("suco-natural").style.borderColor = "white";
-    document.getElementById("whisky").style.borderColor = "green";
-    document.getElementById("coco").style.borderColor = "white";
-    document.getElementById("vinho").style.borderColor = "white";
-    document.getElementById("agua").style.borderColor = "white";
-    plate = "Whisky"
-}
-function chooseCoco() {
-    document.getElementById("coca").style.borderColor = "white";
-    document.getElementById("suco-natural").style.borderColor = "white";
-    document.getElementById("whisky").style.borderColor = "white";
-    document.getElementById("coco").style.borderColor = "green";
-    document.getElementById("vinho").style.borderColor = "white";
-    document.getElementById("agua").style.borderColor = "white";
-    plate = "Água de Côco"
-}
-function chooseWine() {
-    document.getElementById("coca").style.borderColor = "white";
-    document.getElementById("suco-natural").style.borderColor = "white";
-    document.getElementById("whisky").style.borderColor = "white";
-    document.getElementById("coco").style.borderColor = "white";
-    document.getElementById("vinho").style.borderColor = "green";
-    document.getElementById("agua").style.borderColor = "white";
-    plate = "Vinho tinto"
-}
-function chooseWater() {
-    document.getElementById("coca").style.borderColor = "white";
-    document.getElementById("suco-natural").style.borderColor = "white";
-    document.getElementById("whisky").style.borderColor = "white";
-    document.getElementById("coco").style.borderColor = "white";
-    document.getElementById("vinho").style.borderColor = "white";
-    document.getElementById("agua").style.borderColor = "green";
-    plate = "Garrafa de Água"
-}
-
-//terceira escolha
-function choosePudim() {
-    document.getElementById("pudim").style.borderColor = "green";
-    document.getElementById("brigadeiro").style.borderColor = "white";
-    document.getElementById("sorvete").style.borderColor = "white";
-    document.getElementById("yogurt").style.borderColor = "white";
-    document.getElementById("pavê").style.borderColor = "white";
-    document.getElementById("churros").style.borderColor = "white";
-    plate = "Pudim"
-}
-function chooseChocolat() {
-    document.getElementById("pudim").style.borderColor = "white";
-    document.getElementById("brigadeiro").style.borderColor = "green";
-    document.getElementById("sorvete").style.borderColor = "white";
-    document.getElementById("yogurt").style.borderColor = "white";
-    document.getElementById("pavê").style.borderColor = "white";
-    document.getElementById("churros").style.borderColor = "white";
-    plate = "Brigadeiro"
-}
-function chooseIce() {
-    document.getElementById("pudim").style.borderColor = "white";
-    document.getElementById("brigadeiro").style.borderColor = "white";
-    document.getElementById("sorvete").style.borderColor = "green";
-    document.getElementById("yogurt").style.borderColor = "white";
-    document.getElementById("pavê").style.borderColor = "white";
-    document.getElementById("churros").style.borderColor = "white";
-    plate = "Sorvete"
-}
-function chooseYogurt() {
-    document.getElementById("pudim").style.borderColor = "white";
-    document.getElementById("brigadeiro").style.borderColor = "white";
-    document.getElementById("sorvete").style.borderColor = "white";
-    document.getElementById("yogurt").style.borderColor = "green";
-    document.getElementById("pavê").style.borderColor = "white";
-    document.getElementById("churros").style.borderColor = "white";
-    plate = "Yogurt"
-}
-function choosePave() {
-    document.getElementById("pudim").style.borderColor = "white";
-    document.getElementById("brigadeiro").style.borderColor = "white";
-    document.getElementById("sorvete").style.borderColor = "white";
-    document.getElementById("yogurt").style.borderColor = "white";
-    document.getElementById("pavê").style.borderColor = "green";
-    document.getElementById("churros").style.borderColor = "white";
-    plate = "Pavê"
-}
-function chooseChurros() {
-    document.getElementById("pudim").style.borderColor = "white";
-    document.getElementById("brigadeiro").style.borderColor = "white";
-    document.getElementById("sorvete").style.borderColor = "white";
-    document.getElementById("yogurt").style.borderColor = "white";
-    document.getElementById("pavê").style.borderColor = "white";
-    document.getElementById("churros").style.borderColor = "green";
-    plate = "Churros"
+function chooseDessert(elemento) {
+    dessert = document.querySelector(".middle-items-border-three")
+    if(dessert !== null) {
+        dessert.classList.remove("middle-items-border-three")
+    }
+    elemento.classList.toggle("middle-items-border-three")
+    thirdfood = elemento.querySelector(".middle-first-text").innerHTML
 }
